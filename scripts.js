9ad6df927dfda5e91c451377fda129e5a1b1ee86
Alexis Ivan Guarneros Flores <alexisalekey@gmail.com>
2022-06-21 19:03:30 -0500

    primer repo

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// let inputUsuario=prompt("ingrese su nombre de Usuario")
+
+
+// const cuentas=[
+//     {nombre:"mali", saldo:"200", contraseña: "1515"},
+//     {nombre:"gera", saldo:"290", contraseña: "1218"},
+//     {nombre:"maui", saldo:"67", contraseña: "1234"}]
+
+// const{nombre,saldo,contraseña}= cuentas
+//     if(inputUsuario==="mali"|| inputUsuario==="gera"||inputUsuario=== "maui"){
+//         document.write(`Bienvenido ${inputUsuario}`)}
+
+//     else  
+//     {document.write("ingrese un usuario valido")} 
+    
+
+
+
+
+// var nombre= prompt("ingrese")
+// console.log(input.value)
+
+// // if valor ==""
+
+// const input= document.getElementById("name")
+
+// let form= document.getElementById("form")
+// form.addEventListener("submit", function (event)
+// {
+// console.log(input.value)
+// })
+
